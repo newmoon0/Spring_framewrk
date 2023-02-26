3a3e40c769a42bcc133430ec59e535eb12595a4f
HilolaM <hnew.moon@gmail.com>
2023-02-25 23:28:06 -0600

    Dependency injection

--- Sp_03_Core_DI/src/main/java/coreDi/Java.java
+++ Sp_03_Core_DI/src/main/java/coreDi/Java.java
@@ -7,7 +7,16 @@ import org.springframework.stereotype.Component;
 @AllArgsConstructor
 public class Java {
 
+    //Field Injection
+    //@Autowired
+    //OfficeHours officeHours;
+
+    //Constructor Injection
     OfficeHours officehours;
+    //@Autowored
+//    public Java(OfficeHours officehours) {
+//        this.officehours = officehours;
+//    }
 
     public void getTeachHours(){
         System.out.println("teaching hours: "+(20 +officehours.getHours()));
