3d805b22f16c16c5b6912ca196c42f12bbb5b6b5
HilolaM <hnew.moon@gmail.com>
2023-02-26 23:31:50 -0600

    abstraction continue

--- Sp_04_Core_Abstraction/src/main/java/proxy/EmailCommentNotificationProxy.java
+++ Sp_04_Core_Abstraction/src/main/java/proxy/EmailCommentNotificationProxy.java
@@ -1,11 +1,16 @@
 package proxy;
 
 import model.Comment;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
-public class EmailCommentNotificationProxy {
+@Component
+@Qualifier("EMAIL")
+public class EmailCommentNotificationProxy implements CommentNotificationProxy{
 
-    @Override
-    public void sendComment(Comment comment){
-        System.out.println("Sending notification for comment: "+comment.getText());
-    }
+
+        @Override
+        public void sendComment(Comment comment) {
+            System.out.println("Sending notification for comment : " + comment.getText());
+        }
 }
